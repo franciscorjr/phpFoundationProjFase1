da15e9fd13c773e4039e15d1bbc3592257ebf626
Francisco Júnior <francisco_jr@outlook.com>
2017-03-01 14:23:08 -0400

    Adicioando Bootstrap e alterando o menu.php

--- menu.php
+++ menu.php
@@ -1,6 +1,9 @@
 <div>
     <ul>
         <li><a href="index.php">Home</a> </li>
+        <li><a href="empresa.php">Empresa</a> </li>
+        <li><a href="produto.php">Produtos</a> </li>
+        <li><a href="servico.php">Serviços</a> </li>
         <li><a href="contato.php">Contato</a> </li>
     </ul>
 </div>
