c69c383c7286f7598abbc42d79c187347ac8ce7d
Francisco Júnior <francisco_jr@outlook.com>
2017-03-01 10:49:35 -0400

    Adicionando Arquivos do Netbeans ao gitignore

--- index.php
+++ index.php
@@ -1,14 +1,9 @@
 <html>
-<head>
-    <title></title>
-</head>
-<?php require_once("menu.php"); ?>
-<body>
-<?php require_once($_GET["arquivo"]); ?>
-<?php $x = 1;
-$y = 2;
-if ($y > $x) {
-    echo $x;
-} ?>
-</body>
+    <head>
+        <title></title>
+    </head>
+    <?php require_once("menu.php"); ?>
+    <body>
+        <?php require_once($_GET["arquivo"]); ?>
+    </body>
 </html>
