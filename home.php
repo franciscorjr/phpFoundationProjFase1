fa5ee70e72f3a7b57a21ce8ce97926b09f2f1587
Francisco Júnior <francisco_jr@outlook.com>
2017-03-01 15:33:55 -0400

    Criação da Página Home Padrão

--- home.php
+++ home.php
@@ -1,1 +1,5 @@
-<h1>Home!!</h1>
+<!-- Jumbotron -->
+<div class="jumbotron">
+    <h1>Bem Vindo!</h1>
+    <p class="lead">Primeiro projeto do Curso de PHP Foundation da Code Education, Fase 1.</p>
+</div>
