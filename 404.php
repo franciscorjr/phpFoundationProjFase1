8d852f515bebd3a582182133ee6647517c66a23d
Francisco Júnior <francisco_jr@outlook.com>
2017-03-04 08:18:51 -0400

    Adicionando página 404 e tratando os arquivos não existentes usando file_exists()

---
+++ 404.php
@@ -0,0 +1,4 @@
+<div class="jumbotron">
+    <h1>Erro 404 - Página não encontrada</h1>
+    <p class="lead">A URL informada não é válida, <a href="home">clique aqui para ir ao Home</a></p>
+</div>
--- index.php
+++ index.php
@@ -2,6 +2,14 @@
 $rota = parse_url ("http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
 $path = str_replace("/","", $rota['path']);
 
+if (file_exists($path.".php")) {
+    
+} else {
+    header("HTTP/1.0 404 Not Found"); //Funciona em alguns servidores e em outros não, por isso usei os 2
+    header("Status: 404 Not Found");  //Funciona em alguns servidores e em outros não, por isso usei os 2
+    $path = "404";
+}
+
 require_once("myPHPFunctions.php");
 require_once("header.php");
 require_once("menu.php");
