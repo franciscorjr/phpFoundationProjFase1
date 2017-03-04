22002ca35a591dd4521460c398edba076ed623dc
Francisco Júnior <francisco_jr@outlook.com>
2017-03-04 11:17:37 -0400

    Inicio do array

--- index.php
+++ index.php
@@ -4,6 +4,14 @@ require_once("myPHPFunctions.php");
 $path = basename($_SERVER['REQUEST_URI']);
 
 $arquivo_destino = verificaArquivo($path);
+//$arquivo_destino = substr($arquivo_destino,0,10);
+ if ($arquivo_destino == "home.php"){
+     echo "achei";
+     var_dump($arquivo_destino);
+ }else{
+     echo "que onda mano";
+     var_dump($arquivo_destino);
+ }
 
 require_once("header.php");
 require_once("menu.php");
--- myPHPFunctions.php
+++ myPHPFunctions.php
@@ -2,26 +2,53 @@
 
 function verificaRota($path)
 {
+    $checaRota = function ($rota) use ($path) {
+        if ($rota == $path) {
+            if ($path == "home") {
+                $arquivo_destino = "home.php";
+            } else if ($path == "empresa") {
+                $arquivo_destino = "empresa.php";
+            } else if ($path == "produto") {
+                $arquivo_destino = "produto.php";
+            } else if ($path == "servico") {
+                $arquivo_destino = "servico.php";
+            } else if ($path == "contato") {
+                $arquivo_destino = "contato.php";
+            }
+            echo $arquivo_destino;
+        }
+    };
 
-    if ($path == "home") {
-        $arquivo_destino = "home.php";
-    } else if ($path == "empresa") {
-        $arquivo_destino = "empresa.php";
-    } else if ($path == "produto") {
-        $arquivo_destino = "produto.php";
-    } else if ($path == "servico") {
-        $arquivo_destino = "servico.php";
-    } else if ($path == "contato") {
-        $arquivo_destino = "contato.php";
-    }
-    return $arquivo_destino;
+    $rotas = ["home", "empresa", "produto", "servico", "contato"];
+
+    array_walk($rotas, $checaRota);
 }
 
 function verificaArquivo($arquivo)
 {
     if (file_exists($arquivo . ".php")) {
-        $arquivo_destino = verificaRota($arquivo);
-        return $arquivo_destino;
+        $rotas = ["home", "empresa", "produto", "servico", "contato"];
+
+        $checaRota = function ($rota) use ($arquivo) {
+            if ($rota == $arquivo) {
+                if ($arquivo == "home") {
+                    $arquivo_destino = "home.php";
+                } else if ($arquivo == "empresa") {
+                    $arquivo_destino = "empresa.php";
+                } else if ($arquivo == "produto") {
+                    $arquivo_destino = "produto.php";
+                } else if ($arquivo == "servico") {
+                    $arquivo_destino = "servico.php";
+                } else if ($arquivo == "contato") {
+                    $arquivo_destino = "contato.php";
+                }
+                print $arquivo_destino;
+            }
+        };
+
+         array_walk($rotas, $checaRota);
+         return $checaRota;
+
     } else {
         header("HTTP/1.0 404 Not Found"); //Funciona em alguns servidores e em outros não, por isso usei os 2
         header("Status: 404 Not Found");  //Funciona em alguns servidores e em outros não, por isso usei os 2
